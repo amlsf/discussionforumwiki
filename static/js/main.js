157293e603f88b3a1009c60d714d373307badda3
Amy <amy.mae.lee@gmail.com>
2014-04-30 09:01:55 -0700

    stuff

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,11 @@
+var maxId = 135;
+
 var initForum = function () {
 
     console.log(data);
     displayAll(data);
     toggleResponses(data);
+    replyButton();
 };
 
 
@@ -14,6 +17,7 @@ var initForum = function () {
 var displayAll = function(data) {
   // List topics on page
   var topics = data.topics;
+
   for (var i = 0; i < topics.length; i++) {
     var topicsDiv = document.createElement('div');
     $(topicsDiv).attr('class', 'topic');
@@ -22,16 +26,23 @@ var displayAll = function(data) {
     // console.log('should be appending topic: ' + topics[i].topictitle);
     $(topicsDiv)
       .append("<h4><b><a href='#' class='topicLink' id='topic-" + i + "'>" + topics[i].topictitle
-        + "</a></b></h3><div class='line-separator'></div>");
+        + " (" + topics[i].responses.length + (topics[i].responses.length > 1 ? " replies)": " reply)")
+        + "</a></b></h3>"
+        + "<div class='line-separator'></div>");
 
     // List responses on page. Use responsesDiv to tie all responses for specific topic to event handlder in Responses() function
     var responsesDiv = document.createElement('div');
     $(responsesDiv).attr('class', 'responses-' + i);
     $('.discussion').append(responsesDiv);
 
+    var responseChildDict = {};
     // Create div for individual responses
     for (var x = 0; x < topics[i].responses.length; x++) {
-      addFirstResponse(topics[i].responses[x], responsesDiv);
+      if (topics[i].responses[x].depth % 2 === 0) {
+        addDepthResponse(topics[i].responses[x], responsesDiv, 'bubbleblueleft', responseChildDict);
+      } else {
+        addDepthResponse(topics[i].responses[x], responsesDiv, 'bubblegreyleft', responseChildDict);
+      }
 
 
       // var responseWrapper = document.createElement('div');
@@ -44,7 +55,7 @@ var displayAll = function(data) {
       // $(responseWrapper).append(bubbleWrapper);
 
       // var responseContainer = document.createElement('div');
-      // $(responseContainer).attr('class', 'bubbleblue');
+      // $(responseContainer).attr('class', 'bubbleblueleft');
       // $(bubbleWrapper).append(responseContainer);
 
       // var responseInfo = document.createElement('div');
@@ -67,6 +78,74 @@ var displayAll = function(data) {
   }
 };
 
+// Add reponses
+var addDepthResponse = function(response, responsesDiv, bubbleclass, responseChildDict) {
+      var id = response.id;
+      var parentid = response.parentid;
+      var depth = response.depth;
+
+      var responseWrapper = document.createElement('div');
+      $(responseWrapper).attr('class','responsediv').attr('id', 'response-' + id);
+      $(responsesDiv).append(responseWrapper);
+
+      // indent based on depth
+      var indent = response.depth*30;
+      $(responseWrapper).css('margin-left',"+=" + indent);
+
+      // Created response Wrapper to be able to position blankavator image and bubbleWrapper
+      $(responseWrapper).append("<img class='blankavatar' src='../static/img/blank_avatar.jpg'>");
+  
+      var bubbleWrapper = document.createElement('div');
+      $(bubbleWrapper).attr('class', 'bubblewrap');
+      $(responseWrapper).append(bubbleWrapper);
+
+      var responseContainer = document.createElement('div');
+      $(responseContainer).attr('class', bubbleclass);
+      $(bubbleWrapper).append(responseContainer);
+
+      var responseInfo = document.createElement('div');
+      $(responseContainer).append(responseInfo);
+      var responseContent = document.createElement('div');
+      $(responseContainer).append(responseContent);
+      var responseControls = document.createElement('div');
+      $(responseContainer).append(responseControls);
+
+      if (responseChildDict[parentid]) {
+        $(responseChildDict[parentid]).append(responseWrapper);
+      } else {
+        var responseChild = document.createElement('div');
+        $(responseChild).attr('class', 'childrenWrapper');
+        $('#response-' + parentid).after(responseChild);
+        $(responseChild).append(responseWrapper);
+        responseChildDict[parentid] = responseChild;
+      }
+
+      // indent based on depth
+      // var indent = response.depth*30;
+      // $(responseWrapper).css('margin-left',"+=" + indent);
+
+      $(responseInfo)
+          .append('<p class = \'response\'>'
+            + '<b><a href=\'#\'>' + response.author + '</a></b> '
+            + secondsToTime(response.age)
+            + '<br> id: ' + response.id + ' '
+            + 'parentid: ' + response.parentid + ' '
+            + 'depth: ' + response.depth
+            );
+      $(responseContent)
+          .append('<p class = \'response\'>'
+            + response.posttext);
+      $(responseControls)
+        .append("<form class = 'reply'>"
+          + "<input type='text' class='form-control replybox' id='replyform" + id + " name='content'>"
+          + "<button type='button' class='btn btn-primary btn-sm replybutton' data-panelid='replyform" + id + "'>Reply</button>"
+          + "</form>"
+          );
+
+};
+
+        // .append("<div><button type=\"button\" class=\"btn btn-primary btn-xs\" replybutton>Reply</button></div>"
+
 // Event handler to hide all responses when click on topic (preventdefault to prevent from going to href = # in Topics())
 var toggleResponses = function(data) {
   var topics = data.topics;
@@ -77,39 +156,31 @@ var toggleResponses = function(data) {
   });
 };
 
-var addFirstResponse = function(response, responsesDiv) {
+// convert seconds to hours and minutes
+var secondsToTime = function(totalSeconds) {
+  days = Math.floor(totalSeconds / (60*60*24));
+  totalSeconds %= (60*60*24);
+  hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  minutes = Math.floor(totalSeconds / 60);
+  seconds = totalSeconds % 60;
 
-      var responseWrapper = document.createElement('div');
-      $(responsesDiv).append(responseWrapper);
-
-      $(responseWrapper).append("<img class='blankavatar' src='../static/img/blank_avatar.jpg'>");
-  
-      var bubbleWrapper = document.createElement('div');
-      $(bubbleWrapper).attr('class', 'bubblewrap');
-      $(responseWrapper).append(bubbleWrapper);
-
-      var responseContainer = document.createElement('div');
-      $(responseContainer).attr('class', 'bubbleblue');
-      $(bubbleWrapper).append(responseContainer);
-
-      var responseInfo = document.createElement('div');
-      $(responseContainer).append(responseInfo);
-      var responseContent = document.createElement('div');
-      $(responseContainer).append(responseContent);
-
-      $(responseInfo)
-          .append('<p class = \'response\'>'
-            + '<b><a href=\'#\'>' + response.author + '</a></b> '
-            + 'id: ' + response.id + ' '
-            + 'parentid: ' + response.parentid + ' '
-            + 'depth: ' + response.depth + ' '
-            + 'age: ' + response.age
-            );
-      $(responseContent)
-          .append('<p class = \'response\'>'
-            + response.posttext);
+  return "about " + (days > 0 ? days + " day(s) ago":
+    hours > 0 ? hours + " hour(s) ago":
+    minutes > 0 ? minutes + " minute(s) ago":
+    seconds + " second(s) ago"
+    );
 };
 
+
+// Takes in replies
+var replyButton = function(){
+  $('.replybutton').click(function(event) {
+    var panelid = $(this).attr("data-panelid");
+    var inputresponse = $('#'+panelid).val();
+    console.log(inputresponse);
+  });
+};
 
 
 
