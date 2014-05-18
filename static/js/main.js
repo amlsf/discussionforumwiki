afea49a7bec5f3a67b0ec0a14949269e09c914cd
Amy <amy.mae.lee@gmail.com>
2014-05-18 16:13:30 -0700

    initial commit - discussion boxes created, reply function next

--- static/js/main.js
+++ static/js/main.js
@@ -30,7 +30,7 @@ var displayAll = function(data) {
         + "</a></b></h3>"
         + "<div class='line-separator'></div>");
 
-    // List responses on page. Use responsesDiv to tie all responses for specific topic to event handlder in Responses() function
+    // List responses on page. Use responsesDiv to tie all responses for specific topic to event handler in Responses() function
     var responsesDiv = document.createElement('div');
     $(responsesDiv).attr('class', 'responses-' + i);
     $('.discussion').append(responsesDiv);
@@ -43,42 +43,11 @@ var displayAll = function(data) {
       } else {
         addDepthResponse(topics[i].responses[x], responsesDiv, 'bubblegreyleft', responseChildDict);
       }
-
-
-      // var responseWrapper = document.createElement('div');
-      // $(responsesDiv).append(responseWrapper);
-
-      // $(responseWrapper).append("<img class='blankavatar' src='../static/img/blank_avatar.jpg'>");
-  
-      // var bubbleWrapper = document.createElement('div');
-      // $(bubbleWrapper).attr('class', 'bubblewrap');
-      // $(responseWrapper).append(bubbleWrapper);
-
-      // var responseContainer = document.createElement('div');
-      // $(responseContainer).attr('class', 'bubbleblueleft');
-      // $(bubbleWrapper).append(responseContainer);
-
-      // var responseInfo = document.createElement('div');
-      // $(responseContainer).append(responseInfo);
-      // var responseContent = document.createElement('div');
-      // $(responseContainer).append(responseContent);
-
-      // $(responseInfo)
-      //     .append('<p class = \'response\'>'
-      //       + '<b><a href=\'#\'>' + topics[i].responses[x].author + '</a></b> '
-      //       + 'id: ' + topics[i].responses[x].id + ' '
-      //       + 'parentid: ' + topics[i].responses[x].parentid + ' '
-      //       + 'depth: ' + topics[i].responses[x].depth + ' '
-      //       + 'age: ' + topics[i].responses[x].age
-      //       );
-      // $(responseContent)
-      //     .append('<p class = \'response\'>'
-      //       + topics[i].responses[x].posttext);
     }
   }
 };
 
-// Add reponses
+// Add responses
 var addDepthResponse = function(response, responsesDiv, bubbleclass, responseChildDict) {
       var id = response.id;
       var parentid = response.parentid;
@@ -144,7 +113,6 @@ var addDepthResponse = function(response, responsesDiv, bubbleclass, responseChi
 
 };
 
-        // .append("<div><button type=\"button\" class=\"btn btn-primary btn-xs\" replybutton>Reply</button></div>"
 
 // Event handler to hide all responses when click on topic (preventdefault to prevent from going to href = # in Topics())
 var toggleResponses = function(data) {
@@ -185,122 +153,3 @@ var replyButton = function(){
 
 
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-function showActive() {
-  $('.toggle-active .btn').change(function(event) {
-    if ($(event.target).prop("id") === "toggle-active-on") {
-      $.ajax({
-        url: "/leafactivelistings",
-      }).done(function(data) {
-        prices = $.parseJSON(data);
-        createMarkers(prices);
-      });
-    } else {
-      if (markers !== null) {
-        map.removeLayer(markers);
-      }
-    }
-  });
-}
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-// SECTION 
-
-function showHeatMap(region, urli, metric) {
-    if (heatLayerCount === 1) {
-      throw 'WTF';
-    }
-    $.ajax({
-// pulls "data" from the data returned in the path /geoidpricesajax
-      url: urli,
-// .done is a callback, submits function and waits for callback
-      }).done(function(data){
-// extra careful with browser issues
-      if (console && console.log ) {
-// takes JSON data and converts it JS objects 
-        geoIdPrices = $.parseJSON(data);
-// call heatColors AFTER stuff above has loaded
-        $("#radio").removeClass("is-nodisplay");
-        heatColors(region, metric);
-        removeLegend();
-        createLegend();
-      }
-    });
-}
-
-
-// toggle checkbox to show and remove heatmap layer
-function toggleHeatMap(region) {
-  $('.toggle-heat .btn').change(function(event){
-// if toggle button on, show radio button
-    if ($(event.target).prop("id") === "toggle-heat-on") {
-        $("#radio").removeClass("is-nodisplay");
-       if ($("#SPSC").is(":checked")) {
-          $("#year-slider").removeClass("is-nodisplay");
-          $("#slider-range").show();
-            currentMetric = 'change';
-            var values = $('#slider-range').slider('values');
-            growthChange(values[0], values[1], geochanges, zips, currentMetric);
-        } else {
-            heatColors(region,currentMetric);
-            createLegend();
-        }
-// if prices $ change checked at time of toggling, add back slider range
-    } else {
-// removes metrics options if heatmap toggle is unchecked along with layer and legend
-      $("#radio").addClass("is-nodisplay");
-      console.log("removing heat layer");
-      heatLayerCount -= 1;
-      map.removeLayer(heatLayer);
-      removeLegend();
-// if sales price % change checked at time of toggling, removes slider range
-     if ($("#SPSC").is(":checked")) {
-        $("#year-slider").addClass("is-nodisplay");
-        $("#slider-range").hide();
-      }
-    }
-  });
-}
-
-
-function showActive() {
-  $('.toggle-active .btn').change(function(event) {
-    if ($(event.target).prop("id") === "toggle-active-on") {
-      $.ajax({
-        url: "/leafactivelistings",
-      }).done(function(data) {
-        prices = $.parseJSON(data);
-        createMarkers(prices);
-      });
-    } else {
-      map.removeLayer(markers);
-    }
-  });
-}
-
-
-function growthChange(baseyear, compyear, urli, region, metric) {
-    console.log(baseyear);
-    console.log(compyear);
-
-      $.ajax({
-      url: urli,
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify({"baseyear":baseyear, "compyear":compyear})
-      }).done(function(data){
-        geoIdPrices = $.parseJSON(data);
-        heatColors(region, metric);
-        removeLegend();
-        createLegend();
-    });
-}
